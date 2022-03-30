1100b842407665936f16ee5c1dd82cd8fd466f10
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-03-30 18:00:47 +0200

    XcodeSetup
    Files automatically created by Xcode

---
+++ H3N-tLe/H3N-tLe/H3N_tLeApp.swift
@@ -0,0 +1,17 @@
+//
+//  H3N_tLeApp.swift
+//  H3N-tLe
+//
+//  Created by Nikolas Huber on 30.03.22.
+//
+
+import SwiftUI
+
+@main
+struct H3N_tLeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
