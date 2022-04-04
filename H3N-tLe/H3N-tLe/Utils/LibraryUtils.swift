34c1632249fbb3d3173e094b3358df6b286e26ae
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-04-04 09:09:27 +0200

    add getSeriesInfo to LibraryUtils

--- H3N-tLe/H3N-tLe/Utils/LibraryUtils.swift
+++ H3N-tLe/H3N-tLe/Utils/LibraryUtils.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+let fileManager = FileManager.default
+
+// URL to the library directory inside the documents directory
+let libraryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("Library")
+
+// Reads the info.json file of the Series at the given URL
+func getSeriesInfo(at url: URL) -> [String: Any]? {
+    let infoURL = url.appendingPathComponent("info.json")
+    return readJsonFromFile(url: infoURL)
+}
