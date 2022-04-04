1104517526ec9a06c085fcfd2fab6dcad953cad5
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-04-04 08:09:04 +0200

    First design of Library

--- H3N-tLe/H3N-tLe/HomeTab.swift
+++ H3N-tLe/H3N-tLe/HomeTab.swift
@@ -8,13 +8,58 @@
 import SwiftUI
 
 struct HomeTab: View {
+    
+    var series: [SeriesModel] = dummySeriesModel
+    
     var body: some View {
         VStack {
             
-            Spacer()
-            
-            Text("HOME TAB")
-                .font(.largeTitle)
+            List {
+                
+                ForEach(series) { series in
+                    HStack {
+                        
+                        Image(series.imageName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 85)
+                        
+                        VStack(alignment: .leading){
+                            Text(series.title)
+                                .font(.headline)
+                                .lineLimit(1)
+                            Text(series.description)
+                                .font(.body)
+                                .lineLimit(2)
+                            HStack {
+                                Button {
+                                    // TODO: set popup variable to true and write index of item to a variable
+                                } label: {
+                                    Text("Read First")
+                                }
+                                .padding()
+                                .background(Color.red)
+                                .foregroundColor(Color.primary)
+                                .cornerRadius(15)
+                                
+                                Spacer()
+                                
+                                Button {
+                                    // TODO: set popup variable to true and write index of item to a variable
+                                } label: {
+                                    Text("Continue")
+                                }
+                                .padding()
+                                .background(Color.green)
+                                .foregroundColor(Color.primary)
+                                .cornerRadius(15)
+
+
+                            }
+                        }
+                    }
+                }
+            }
             
             Spacer()
             
@@ -27,5 +72,29 @@ struct HomeTab: View {
             Text("Library")
         }
         .tag(1)
+    }
+}
+
+struct SeriesModel: Identifiable {
+    var id = UUID()
+    
+    var title: String
+    var description: String
+    var imageName: String
+}
+
+let dummySeriesModel = [SeriesModel(title:"A beautiful title",
+                                    description: "A very well thought out description which describes the series in a very correct and inviting manner",
+                                    imageName: "aNiceThumbNail"),
+                        SeriesModel(title: "Short title",                   description: "A short desctiption",
+                                    imageName: "aSquareThumbNail")]
+
+struct HomeTab_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+            .previewDevice("iPhone 11")
+            .previewLayout(.sizeThatFits)
+            
     }
 }
