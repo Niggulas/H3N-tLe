7b2464540a9387e07820c2df46f22289a91a00c0
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-05-11 14:33:38 +0200

    A very protuctive time

---
+++ H3N-tLe/H3N-tLe/Reader/Reader.swift
@@ -0,0 +1,20 @@
+//
+//  Reader.swift
+//  H3N-tLe
+//
+//  Created by Nikolas Huber on 11.05.22.
+//
+
+import SwiftUI
+
+struct Reader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Reader_Previews: PreviewProvider {
+    static var previews: some View {
+        Reader()
+    }
+}
