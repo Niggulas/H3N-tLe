a1e616e4e95d15ae5796fa16381c637f4b87d88b
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-06-22 17:22:01 +0200

    Information on Trust

---
+++ H3N-tLe/H3N-tLe/SettingsTab/InformationOnTrust.swift
@@ -0,0 +1,20 @@
+//
+//  InformationOnTrust.swift
+//  H3N-tLe
+//
+//  Created by Nikolas Huber on 22.06.22.
+//
+
+import SwiftUI
+
+struct InformationOnTrust: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InformationOnTrust_Previews: PreviewProvider {
+    static var previews: some View {
+        InformationOnTrust()
+    }
+}
