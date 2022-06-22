d28f1e4a4c8ce1442f594eec002483342f940410
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-06-22 19:53:52 +0200

    A small text about password security

--- H3N-tLe/H3N-tLe/SettingsTab/InformationOnTrust.swift
+++ H3N-tLe/H3N-tLe/SettingsTab/InformationOnTrust.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct InformationOnTrust: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		VStack {
+			Text("Be careful with your passwords\n")
+				.font(.title)
+			Text("If the website of which you want to download something requires you to sign in to an account, please make shure that the plugin you use is from a trust worthy developer and or a publisher.\n\nIt is a good sign if the plugin is completely open source, but even that doesn't provide guaranteed safety as long as you don't understand every line of it's code.\n\nIf you don't trust us, feel free to check out the source code for this app in GitHub\n\nAnd if you just can't trust anybody or you are interested in coding, go ahead and develope your own plugins.\nAnd when they work, you can publish them on git hub for other people to use and to contribute something to this project")
+				
+			
+			Spacer()
+		}
     }
 }
 
 struct InformationOnTrust_Previews: PreviewProvider {
     static var previews: some View {
-        InformationOnTrust()
+		InformationOnTrust()
+			.preferredColorScheme(.dark)
+			.previewDevice("iPhone 11")
+			
     }
 }
