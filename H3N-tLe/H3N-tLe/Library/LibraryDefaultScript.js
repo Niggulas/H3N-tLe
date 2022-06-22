fbf8e79400677a8df1b45335d92312a07c205994
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-06-22 17:46:32 +0200

    Finish LibraryDefaultScript.js

--- H3N-tLe/H3N-tLe/Library/LibraryDefaultScript.js
+++ H3N-tLe/H3N-tLe/Library/LibraryDefaultScript.js
@@ -1,89 +1,44 @@
-Object.defineProperty(window, 'Chapter', {
+// Function to check if the series already present in the library
+Object.defineProperty(window, 'doesSeriesExist', {
 	configurable: false,
 	enumerable: true,
 	writable: false,
-	value: class {
-		constructor(chapterName, urls, { title, description, ... }) {
-			// Check series info
-	
-			// Check chapterName
-			if (typeof chapterName !== 'string') {
-				throw new Error(`Invalid value for chapterName: ${chapterName}`);
-			}
-			chapterName = chapterName.trim().replace(/\n|\r/gi, '');
-			if (chapterName.length === 0) {
-				throw new Error('chapterName is too short (legnth 0)');
-			}
-			this.#name = chapterName;
-			
-			// Check urls
-			if (!Array.isArray(urls)) {
-				throw new Error('urls list is not an Array');
-			}
-			// Make sure all entries of urls are strings, valid URLs and use http or https
-			for (let i = 0; i < urls.length; i++) {
-				if (urls[i] instanceof URL) {
-					urls[i] = urls[i].href;
-				} else if (typeof urls[i] !== 'string') {
-					throw new Error(`Invalid value for urls[${i}]: ${urls[i]}`);
-				} else if (urls[i].length < 10) {
-					throw new Error(`urls[${i}] is too short (legnth < 10)`);
+	value: (seriesName) => {
+		if (typeof seriesName !== 'string') {
+			throw new Error('Series name must be a String');
+		} else if (seriesName.length < 1) {
+			throw new Error("Series name can't be empty");
+		}
+
+		return new Promise((resolve, reject) => {
+			window.postWebKitMessage(['DoesSeriesExist', seriesName], true).then((state) => {
+				if (state === 'present') {
+					resolve(true);
+					return;
+				} else if (state === 'absent') {
+					resolve(false);
+					return;
 				}
-	
-				try {
-					urls[i] = new URL(urls[i]);
-				} catch (e) {
-					throw new Error(`urls[${i}] is not a URL: ${urls[i]}`);
-				}
-	
-				if (urls[i].protocol.match(/^https?$/i) === null) {
-					throw new Error(`urls[${i}] is not a HTTP(S) URL: ${urls[i].href}`);
-				}
-	
-				urls[i] = urls[i].href;
-			}
-			this.#urls = urls;
-		}
-	
-		#series;
-		#name;
-		#urls;
-	
-		getSeries() {
-			return this.#series;
-		}
-	
-		getName() {
-			return this.#name;
-		}
-	
-		getUrls() {
-			return this.#urls;
-		}
-	
-		getJSON() {
-			return JSON.stringify({
-				series: this.#series,
-				name: this.#name,
-				urls: this.#urls
+				reject(new Error('Unkown series state: ' + state));
 			});
-		}
+		});
 	}
 });
 
+// Function to download a chapter and send some other information to swift (url to next chapter, description of the series, ...)
 Object.defineProperty(window, 'downloadChapter', {
 	configurable: false,
 	enumerable: true,
 	writable: false,
 	value: (chapterInfo) => {
 		if (
-			typeof chapterInfo?.getSeries()?.title !== 'string' ||
-			typeof chapterInfo?.getName() !== 'string' ||
-			!Array.isArray(chapterInfo?.getUrls()) ||
-			typeof chapterInfo?.getJSON() !== 'string'
+			typeof chapterInfo !== 'object' ||
+			typeof chapterInfo?.series?.title !== 'string' ||
+			typeof chapterInfo?.name !== 'string' ||
+			!Array.isArray(chapterInfo?.urls)
 		) {
 			throw new Error('Invalid chapterInfo');
 		}
-		window.postWebKitMessage(['DownlaodChapter', chapterInfo.getJSON()]);
+		window.postWebKitMessage(['DownlaodChapter', JSON.stringify(chapterInfo)]);
 	}
 });
