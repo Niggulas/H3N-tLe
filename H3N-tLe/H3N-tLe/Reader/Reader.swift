b92bee3f1c5418f64cb52b846adb6802d79a1c8e
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-06-22 13:39:21 +0200

    add comments

--- H3N-tLe/H3N-tLe/Reader/Reader.swift
+++ H3N-tLe/H3N-tLe/Reader/Reader.swift
@@ -28,6 +28,8 @@ struct Reader: View {
         ScrollView (.vertical, showsIndicators: false, content: {
             // Top Buttons
             HStack {
+                
+                // Check if it is the firs chapter, if not, then show a previous chapter button
                 if !isFirstChapter() {
                     Button {
                         chapterIndex -= 1
@@ -41,9 +43,11 @@ struct Reader: View {
                     .cornerRadius(15)
                 }
                 
+                // Check if it is the last chapter, if not, then show a next chapter button
                 if !isLastChapter() {
                     Button {
                         chapterIndex += 1
+                        series.markChapterAsRead(chapter: series.getChapterList()[chapterIndex])
                     } label: {
                         Image(systemName: "arrow.right")
                             .frame(minWidth: 1, maxWidth: .infinity, minHeight: 20)
@@ -66,14 +70,14 @@ struct Reader: View {
                         ProgressView()
                     }
                     .aspectRatio(contentMode: .fit)
-                    .frame(minWidth: 1, idealWidth: .infinity, maxWidth: .infinity) //Komische dinge passieren
-                    // Image wird breiter geladen, als der screen weit ist
-                    // muss schauen wie man das auf den screen begrenzt bekommt
+                    .frame(minWidth: 1, idealWidth: .infinity, maxWidth: .infinity)
                 }
             }
             
             // Bottom Buttons
             HStack {
+                
+                // Check if it is the firs chapter, if not, then show a previous chapter button
                 if !isFirstChapter() {
                     Button {
                         chapterIndex -= 1
@@ -87,9 +91,11 @@ struct Reader: View {
                     .cornerRadius(15)
                 }
                 
+                // Check if it is the last chapter, if not, then show a next chapter button
                 if !isLastChapter() {
                     Button {
                         chapterIndex += 1
+                        series.markChapterAsRead(chapter: series.getChapterList()[chapterIndex])
                     } label: {
                         Image(systemName: "arrow.right")
                             .frame(minWidth: 1, maxWidth: .infinity, minHeight: 20)
@@ -102,12 +108,17 @@ struct Reader: View {
                 
             }
             
-            
         })
-        //.ignoresSafeArea()
-        .tabItem {
-            Image(systemName: "eyeglasses")
-            Text("Reader")
+        /*
+         if there is no chapter afterwards, you can't use the next chapter button to check if the user read the chapter
+         so you need to check if it was opened
+         */
+        .onAppear {
+            if isLastChapter() {
+                series.markChapterAsRead(chapter: series.getChapterList()[chapterIndex])
+            }
         }
+        
     }
+    
 }
--- H3N-tLe/H3N-tLe/SettingsTab/PluginList.swift
+++ H3N-tLe/H3N-tLe/SettingsTab/PluginList.swift
@@ -9,20 +9,29 @@ import SwiftUI
 
 struct PluginList: View {
     
+    // Get an array of currently installed plugins
+    @State var plugInList = plugInManager.getAllPlugInNames()
+    
     var body: some View {
+        
+        // Use of a list, so that the plugins get displayed in a organized way
         List {
-            ForEach(0..<20) { plugin in
-                Link(destination: URL(string: "https://google.com")!,
-                     label: {
-                    Label("Plugin \(plugin)", systemImage: "link")
-                    
+            
+            // Loop that goes throu the array of installed plugins
+            ForEach(plugInList.map { IdentifieableAny(value: $0) } ) { plugInName in
+                
+                // The Elements are Links, so they can lead to their repo
+                Link(destination: URL(string: "example.com")!, label: { // TODO: make the Link dynamic to the website of the plugin
+                    Label(plugInName.value as! String, systemImage: "link")
                 })
                 
             }
+            // this makes the elements able to be swiped left to delete them
             .onDelete(perform: {
                 indexSet in
                 
-                
+                PlugInManager.deletePlugIn(<#T##self: PlugInManager##PlugInManager#>)
+                // TODO: write action to remove plugin
             })
             
         }
