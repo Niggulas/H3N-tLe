0e34521cdddf61035258ad524df8dba4a3af6cf8
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-06-23 12:43:03 +0200

    status for series in library

--- H3N-tLe/H3N-tLe/LibraryTab/LibraryTab.swift
+++ H3N-tLe/H3N-tLe/LibraryTab/LibraryTab.swift
@@ -9,102 +9,148 @@ import SwiftUI
 import WebKit
 
 struct LibraryTab: View {
-    
-    @State var seriesList = library.getSeriesList()
-    
-    var body: some View {
-        VStack {
-            NavigationView {
-                
-                    Form{
-                        ForEach(seriesList) { series in
-                            // Series Element
-                            NavigationLink(destination: SeriesView(series: series), label: {
-                                
-                                HStack{
-                                    if series.getCoverUrl() != nil {
-                                        AsyncImage(url: series.getCoverUrl()) { phase in
-                                            if let image = phase.image {
-                                                image
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fit)
-                                                    .frame(width: 85)
-                                                    .cornerRadius(5)
-                                            } else if phase.error != nil {
-                                                Image("Cover")
-                                                    .resizable()
-                                                    .aspectRatio(contentMode: .fit)
-                                                    .frame(width: 85)
-                                                    .cornerRadius(5)
-                                            } else {
-                                                Color(.systemGray6)
-													.frame(width: 85, height: 128)
-                                            }
-                                        }
-                                    } else {
-                                        Image("Cover")
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 85)
-                                            .cornerRadius(5)
-                                    }
-                                }
-                                
-                                VStack (alignment: .leading){
-                                    Text(series.title)
-                                        .font(.headline)
-                                        .lineLimit(1)
-                                    
-                                    Text(series.description)
-                                        .foregroundColor(Color.secondary)
-                                        .font(.body)
-                                        .lineLimit(2)
+	
+	@State var seriesList = library.getSeriesList()
+	
+	var body: some View {
+		VStack {
+			NavigationView {
+				
+				Form{
+					ForEach(seriesList) { series in
+						// Series Element
+						NavigationLink(destination: SeriesView(series: series), label: {
+							
+							// Cover image with option to 
+							HStack{
+								if series.getCoverUrl() != nil {
+									AsyncImage(url: series.getCoverUrl()) { phase in
+										if let image = phase.image {
+											image
+												.resizable()
+												.aspectRatio(contentMode: .fit)
+												.frame(width: 85)
+												.cornerRadius(5)
+										} else if phase.error != nil {
+											Image("Cover")
+												.resizable()
+												.aspectRatio(contentMode: .fit)
+												.frame(width: 85)
+												.cornerRadius(5)
+										} else {
+											Color(.systemGray6)
+												.frame(width: 85, height: 128) // to keep the correct sizing when you delete an item
+										}
+									}
+								} else {
+									Image("Cover")
+										.resizable()
+										.aspectRatio(contentMode: .fit)
+										.frame(width: 85)
+										.cornerRadius(5)
+								}
+							}
+							
+							// Series text & status
+							VStack (alignment: .leading){
+								
+								// Title
+								Text(series.title)
+									.font(.headline)
+									.lineLimit(1)
+								
+								// Description
+								Text(series.description)
+									.foregroundColor(Color.secondary)
+									.font(.body)
+									.lineLimit(2)
+								
+								// new line to seperate the status from the description and b/c .padding() wasn't the right thing
+								Text("")
+								
+								// Show the status of the series in a color corralating to the status
+								switch series.getStatus() {
+									case "dropped" :
+										Text(series.getStatus())
+											.font(.body)
+											.lineLimit(1)
+											.foregroundColor(.red)
+											.background(Color(.systemGray5))
+											.cornerRadius(5)
 									
-                                }
-                            })
-                            
-                        }
-						.onDelete(perform: { index in
-							seriesList[index.first!].delete()
-							library.updateSeriesList()
-							seriesList = library.getSeriesList()
+									case "ongoing" :
+										Text(series.getStatus())
+											.font(.body)
+											.lineLimit(1)
+											.foregroundColor(.green)
+											.background(Color(.systemGray5))
+											.cornerRadius(5)
+									
+									case "hiatus" :
+										Text(series.getStatus())
+											.font(.body)
+											.lineLimit(1)
+											.foregroundColor(.yellow)
+											.background(Color(.systemGray5))
+											.cornerRadius(5)
+										
+									case "finished" :
+										Text(series.getStatus())
+											.font(.body)
+											.lineLimit(1)
+											.foregroundColor(.cyan)
+											.background(Color(.systemGray5))
+											.cornerRadius(5)
+									
+									default :
+										Text(series.getStatus())
+											.font(.body)
+											.lineLimit(1)
+											.foregroundColor(.red)
+											.background(Color(.systemGray5))
+											.cornerRadius(5)
+								}
+								
+							}
+							
 						})
-                    }
-                    .navigationTitle("Library")
-					.toolbar {
-						Button {
-							library.updateSeriesList()
-							seriesList = library.getSeriesList()
-						} label: {
-							Text("Refresh")
-								.foregroundColor(.red)
-						}
-
+						
 					}
-                    
-            }
-            
-        }
-        .tabItem {
-            Image(systemName: "books.vertical")
-            Text("Library")
-        }
-        .tag(1)
+					.onDelete(perform: { index in
+						seriesList[index.first!].delete()
+						library.updateSeriesList()
+						seriesList = library.getSeriesList()
+					})
+				}
+				.navigationTitle("Library")
+				.toolbar {
+					Button {
+						library.updateSeriesList()
+						seriesList = library.getSeriesList()
+					} label: {
+						Text("Refresh")
+							.foregroundColor(.red)
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		// define symbol and text for the library element in the tab bar
+		.tabItem {
+			Image(systemName: "books.vertical")
+			Text("Library")
+		}
+		// use this tab if you open the app
+		.tag(1)
+		// auto refresh the library if you go to another tab
 		.onDisappear(perform: {
 			library.updateSeriesList()
 			seriesList = library.getSeriesList()
 		})
-        
-    }
-    
-}
-
-struct HomeTab_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
-            .previewDevice("iPhone 11")
-            .previewLayout(.sizeThatFits)
-            
-    }
+		
+	}
+	
 }
