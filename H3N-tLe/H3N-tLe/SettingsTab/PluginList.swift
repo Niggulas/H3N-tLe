55433d3c2d1f638e953af68a653fe9ecd74f208f
Niggulas <79905466+Niggulas@users.noreply.github.com>
2022-06-23 12:44:11 +0200

    comments in pluginList

--- H3N-tLe/H3N-tLe/SettingsTab/PluginList.swift
+++ H3N-tLe/H3N-tLe/SettingsTab/PluginList.swift
@@ -8,45 +8,45 @@
 import SwiftUI
 
 struct PluginList: View {
-    
-    // Get an array of currently installed plugins
-    @State var plugInList = plugInManager.getAllPlugInNames()
-    
-    var body: some View {
-        
-        // Use of a list, so that the plugins get displayed in a organized way
-        List {
-            // Loop that goes throu the array of installed plugins
-            ForEach(plugInList.map { IdentifieableAny(value: $0) } ) { plugInName in
-                
-                if let url = plugInManager.getPlugInWebsite(plugIn: plugInName.value as! String) {
-                    // The Elements are Links, so they can lead to their repo
-                    Link(destination: url, label: { // TODO: make the Link dynamic to the website of the plugin
-                        HStack{
-                            Text(plugInName.value as! String)
-                            Spacer()
-                            Image(systemName: "link")
-                        }
-                    })
-                    
-                    
-                } else {
-                    Text(plugInName.value as! String)
-                        .foregroundColor(.red)
-                }
-                
-                
-            }
-            
-        }
-        
-    }
-    
-}
-
-struct PluginList_Previews: PreviewProvider {
-    static var previews: some View {
-        PluginList()
-            .preferredColorScheme(.dark)
-    }
+	
+	// Get an array of currently installed plugins
+	@State var plugInList = plugInManager.getAllPlugInNames()
+	
+	var body: some View {
+		
+		// Use of a list, so that the plugins get displayed in a organized way
+		List {
+			
+			// Loop that goes through the array of installed plugins
+			ForEach(plugInList.map { IdentifieableAny(value: $0) } ) { plugInName in
+				
+				/*
+				 if the .json file of the plugin containes a link, you can tap the element to ofen the link
+				 and in the right side of the element a "link" icon will apear
+				 */
+				if let url = plugInManager.getPlugInWebsite(plugIn: plugInName.value as! String) {
+					// The Elements are Links, so they can lead to their repo
+					Link(destination: url, label: {
+						HStack{
+							Text(plugInName.value as! String)
+							Spacer()
+							Image(systemName: "link")
+						}
+						
+					})
+					
+				}
+				
+				// if the .json file doesn't contain a link the element just contains the Name of the Plugin
+				else {
+					Text(plugInName.value as! String)
+						.foregroundColor(.red)
+				}
+				
+			}
+			
+		}
+		
+	}
+	
 }
